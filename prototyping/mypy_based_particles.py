9e7737994571e74d4eb2f0333dae0e3fa67e2d83
SimonBiggs <me@simonbiggs.net>
2021-01-03 14:11:43 +1100

    plot with some tracks included

--- prototyping/mypy_based_particles.py
+++ prototyping/mypy_based_particles.py
@@ -98,13 +98,26 @@ def main():
     num_particles = int(1e6)
     iterations = 10
     runs = 10
+    num_particles_to_plot = 100
 
     particles = particles_zeros(num_particles)
 
+    positions_for_plotting = []
     for _ in range(runs):
         prng_key, particles = random_walk(prng_key, particles, iterations)
+        positions_for_plotting.append(
+            particles["position"][0:2, 0:num_particles_to_plot]
+        )
 
-    plt.scatter(particles["position"][0, 0:1000], particles["position"][1, 0:1000])
+    positions_for_plotting = jnp.array(positions_for_plotting)
+
+    plt.plot(
+        positions_for_plotting[:, 0, :],
+        positions_for_plotting[:, 1, :],
+        "o-",
+        alpha=0.7,
+    )
+    plt.axis("equal")
     plt.show()
 
 
